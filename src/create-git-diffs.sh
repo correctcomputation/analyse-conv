03dd63daa89339518329f9fa30bd9bc9b399b13d
Kyle Headley <kyle@correctcomputation.com>
2021-04-27 16:56:20 -0400

    only add relavant changes

--- src/create-git-diffs.sh
+++ src/create-git-diffs.sh
@@ -13,31 +13,31 @@ fi
 git checkout -B $NAME-diff-orig
 mkdir -p git-changes
 if cp orig/* git-changes/ ; then
-	git add --all
+	git add git-changes/*
 	git commit -m"$NAME: setup originals"
 	[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-orig
 
 	git checkout -B $NAME-diff-3c-orig
 	if cp 3c-orig/* git-changes ; then
-		git add --all
+		git add git-changes/*
 		git commit -m"$NAME: convert to 3c version"
 		[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-3c-orig
 
 		git checkout -B $NAME-diff-manual
 		if cp manual/* git-changes ; then
-			git add --all
+			git add git-changes/*
 			git commit -m"$NAME: replace with manual version"
 			[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-manual
 
 			git checkout -B $NAME-diff-revert
 			if cp revert/* git-changes ; then
-				git add --all
+				git add git-changes/*
 				git commit -m"$NAME: remove checkedc annotations"
 				[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-revert
 
 				git checkout -B $NAME-diff-3c-revert
 				if cp 3c-revert/* git-changes ; then
-					git add --all
+					git add git-changes/*
 					git commit -m"$NAME: reconvert to 3c version"
 					[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-3c-revert
 				fi
