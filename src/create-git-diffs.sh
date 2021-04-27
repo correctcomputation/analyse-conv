1a25526385d3c28e4484a9050bb314be91f06baa
Kyle Headley <kyle@correctcomputation.com>
2021-04-27 16:54:31 -0400

    proper use of vars

--- src/create-git-diffs.sh
+++ src/create-git-diffs.sh
@@ -15,31 +15,31 @@ mkdir -p git-changes
 if cp orig/* git-changes/ ; then
 	git add --all
 	git commit -m"$NAME: setup originals"
-	[ -v PUSH ] && git push --set-upstream origin $NAME-diff-orig
+	[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-orig
 
 	git checkout -B $NAME-diff-3c-orig
 	if cp 3c-orig/* git-changes ; then
 		git add --all
 		git commit -m"$NAME: convert to 3c version"
-		[ -v PUSH ] && git push --set-upstream origin $NAME-diff-3c-orig
+		[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-3c-orig
 
 		git checkout -B $NAME-diff-manual
 		if cp manual/* git-changes ; then
 			git add --all
 			git commit -m"$NAME: replace with manual version"
-			[ -v PUSH ] && git push --set-upstream origin $NAME-diff-manual
+			[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-manual
 
 			git checkout -B $NAME-diff-revert
 			if cp revert/* git-changes ; then
 				git add --all
 				git commit -m"$NAME: remove checkedc annotations"
-				[ -v PUSH ] && git push --set-upstream origin $NAME-diff-revert
+				[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-revert
 
 				git checkout -B $NAME-diff-3c-revert
 				if cp 3c-revert/* git-changes ; then
 					git add --all
 					git commit -m"$NAME: reconvert to 3c version"
-					[ -v PUSH ] && git push --set-upstream origin $NAME-diff-3c-revert
+					[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-3c-revert
 				fi
 			fi
 		fi
