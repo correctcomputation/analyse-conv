b3ff70c83cb7363e964babd1ffdec0682ad38934
Kyle Headley <kyle@correctcomputation.com>
2021-04-27 17:07:10 -0400

    try not to push

--- src/create-git-diffs.sh
+++ src/create-git-diffs.sh
@@ -2,7 +2,7 @@
 
 # requires NAME
 
-#PUSH=
+PUSH= #set to anything to push changes, set to nothing to skip push
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if ! git diff-index --quiet HEAD --; then
@@ -15,31 +15,31 @@ mkdir -p git-changes
 if cp orig/* git-changes/ ; then
 	git add git-changes/*
 	git commit -m"$NAME: setup originals"
-	[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-orig
+	[ ! -z $PUSH ] && git push --set-upstream origin $NAME-diff-orig
 
 	git checkout -B $NAME-diff-3c-orig
 	if cp 3c-orig/* git-changes ; then
 		git add git-changes/*
 		git commit -m"$NAME: convert to 3c version"
-		[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-3c-orig
+		[ ! -z $PUSH ] && git push --set-upstream origin $NAME-diff-3c-orig
 
 		git checkout -B $NAME-diff-manual
 		if cp manual/* git-changes ; then
 			git add git-changes/*
 			git commit -m"$NAME: replace with manual version"
-			[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-manual
+			[ ! -z $PUSH ] && git push --set-upstream origin $NAME-diff-manual
 
 			git checkout -B $NAME-diff-revert
 			if cp revert/* git-changes ; then
 				git add git-changes/*
 				git commit -m"$NAME: remove checkedc annotations"
-				[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-revert
+				[ ! -z $PUSH ] && git push --set-upstream origin $NAME-diff-revert
 
 				git checkout -B $NAME-diff-3c-revert
 				if cp 3c-revert/* git-changes ; then
 					git add git-changes/*
 					git commit -m"$NAME: reconvert to 3c version"
-					[ -v $PUSH ] && git push --set-upstream origin $NAME-diff-3c-revert
+					[ ! -z $PUSH ] && git push --set-upstream origin $NAME-diff-3c-revert
 				fi
 			fi
 		fi
