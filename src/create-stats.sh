81512f9d513c70b91797c62784be514cddbf8840
Kyle Headley <kyle@correctcomputation.com>
2021-04-28 14:02:45 -0400

    better cleaning; fix file format

--- src/create-stats.sh
+++ src/create-stats.sh
@@ -7,7 +7,7 @@ for pair in "3c-orig>manual" "orig>3c-orig" "manual>revert" "revert>3c-revert" ;
 	if [ -d $first ] && [ -d $second ] ; then
 		echo "# $first-to-$second" >> diffs.dat
 		diff $first $second | diffstat -t -S$first | sed '1d' >> diffs.dat
-		echo >> diffs.dat
+		echo -e "\n" >> diffs.dat # this is 2 spaces, as required
 	fi
 done
 
