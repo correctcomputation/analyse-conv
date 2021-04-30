997b97c96998b74ad0e539400c6aaaf1b72d6d76
Kyle Headley <kyle@correctcomputation.com>
2021-04-30 18:52:44 -0400

    combine stats into parent directory

--- src/create-stats.sh
+++ src/create-stats.sh
@@ -4,7 +4,7 @@ declare -A summation
 
 echo "INSERTED,DELETED,MODIFIED,UNCHANGED,FILENAME" > diffs.dat
 echo "INSERTED,DELETED,MODIFIED,UNCHANGED,FILENAME" > diffs.sum
-for pair in "3c-orig>manual" "orig>3c-orig" "manual>revert" "revert>3c-revert" ; do
+for pair in "orig>3c-orig" "3c-orig>manual" "manual>revert" "revert>3c-revert" ; do
 	first=$(echo $pair | cut -d ">" -f 1)
 	second=$(echo $pair | cut -d ">" -f 2)
 	if [ -d $first ] && [ -d $second ] ; then
@@ -15,13 +15,15 @@ for pair in "3c-orig>manual" "orig>3c-orig" "manual>revert" "revert>3c-revert" ;
 		summation[del]=0
 		summation[mod]=0
 		summation[unc]=0
-		while IFS=, read -r ins del mod unc fil; do
-			summation[ins]=$((summation[ins]+$ins))
-			summation[del]=$((summation[del]+$del))
-			summation[mod]=$((summation[mod]+$mod))
-			summation[unc]=$((summation[unc]+$unc))
-		done < <(echo -e "$lines")
-		echo -e "$lines" >> diffs.dat
+		if [[ ! -z $lines ]]; then
+			while IFS=, read -r ins del mod unc fil; do
+				summation[ins]=$((summation[ins]+$ins))
+				summation[del]=$((summation[del]+$del))
+				summation[mod]=$((summation[mod]+$mod))
+				summation[unc]=$((summation[unc]+$unc))
+			done < <(echo -e "$lines")
+			echo -e "$lines" >> diffs.dat
+		fi
 		echo -e "${summation[ins]},${summation[del]},${summation[mod]},${summation[unc]},$NAME" \
 			 >> diffs.sum
 		echo -e "\n" >> diffs.dat # this is 2 spaces, as required
