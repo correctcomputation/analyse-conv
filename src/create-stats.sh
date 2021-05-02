deef3925983c73472a8daa0c1dad25ad390da64a
Aaron Eline <aaron@correctcomputation.com>
2021-05-02 15:23:17 -0400

    Changed how sum is computed

--- src/create-stats.sh
+++ src/create-stats.sh
@@ -28,16 +28,13 @@ for pair in "orig>revert" "revert>manual" "revert>3c-revert" "orig>3c-orig" ; do
 		summation[mod]=0
 		summation[unc]=0
 		if [[ ! -z $lines ]]; then
-			while IFS=, read -r ins del mod unc fil; do
-				summation[ins]=$((summation[ins]+$ins))
-				summation[del]=$((summation[del]+$del))
-				summation[mod]=$((summation[mod]+$mod))
-				summation[unc]=$((summation[unc]+$unc))
-			done < <(echo -e "$lines")
+      ins=$(echo $lines | awk 'BEGIN { FS = "," } { print $1 }')
+      del=$(echo $lines | awk 'BEGIN { FS = "," } { print $2 }')
+      chg=$(echo $lines | awk 'BEGIN { FS = "," } { print $3 }')
+      total=$(echo "$ins $del $chg + p" | dc)
 			echo -e "$lines" >> diffs.dat
+      echo -e "$total" >> diffs.sum
 		fi
-		echo -e "${summation[ins]},${summation[del]},${summation[mod]},${summation[unc]},$NAME" \
-			 >> diffs.sum
 		echo -e "\n" >> diffs.dat # this is 2 spaces, as required
 		echo -e "\n" >> diffs.sum # this is 2 spaces, as required
 	fi
