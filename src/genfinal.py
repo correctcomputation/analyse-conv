7c0392bc86e25d70b4a9d4dfdf5ed745057decb2
mwhicks1 <mwh@correctcomputation.com>
2021-05-05 07:06:22 -0400

    Added lines left back to table

--- src/genfinal.py
+++ src/genfinal.py
@@ -13,10 +13,10 @@ from typing import List
 # TEST,REFACTOR_LINES,REFACTOR_TOTAL,ANNOTATED_LINES,ANNOTATED_TOTAL,LEFT_LINES,LEFT_TOTAL
 #  0  |      1       |     2        |        3      |      4       |
 
-# TODO: Omitting "lines left" from table
+# NOTE: uncomment/swap to omit lines left from table
 # Output format:
-#outheader = ['Program', 'Version', 'Lines Refactored', 'Lines Annotated', 'Lines Left',  '#ptr', '#ntarr', '#arr', '#wild', '#bounds', '#casts']
-outheader = ['Program', 'Version', 'Lines Refactored', 'Lines Annotated', '#ptr', '#ntarr', '#arr', '#wild', '#bounds', '#casts']
+outheader = ['Program', 'Version', 'Lines Refactored', 'Lines Annotated', 'Lines Left',  '#ptr', '#ntarr', '#arr', '#wild', '#bounds', '#casts']
+#outheader = ['Program', 'Version', 'Lines Refactored', 'Lines Annotated', '#ptr', '#ntarr', '#arr', '#wild', '#bounds', '#casts']
 # If run w/ no arguments, just output the header
 if len(sys.argv) < 2:
     print(','.join(outheader))
@@ -30,11 +30,13 @@ def process_row(row : List[str]) -> str:
     row = row_to_int(row)
     version = row[0] 
     if version == 'manual':
-#        out = [program, version, compute_percentage(row[1], row[2]), compute_percentage(row[3], row[4]), 'N/A'] + loadstats(version)
-        out = [program, version, compute_percentage(row[1], row[2]), compute_percentage(row[3], row[4])] + loadstats(version)
+        out = [program, version, compute_percentage(row[1], row[2]), compute_percentage(row[3], row[4]), 'N/A'] + loadstats(version)
+# NOTE: uncomment/swap to omit lines left from table
+#        out = [program, version, compute_percentage(row[1], row[2]), compute_percentage(row[3], row[4])] + loadstats(version)
     else:
-#        out = [program, version, 'N/A', compute_percentage(row[3], row[4]), compute_percentage(row[5], row[6])] + loadstats(version)
-        out = [program, version, 'N/A', compute_percentage(row[3], row[4])] + loadstats(version)
+        out = [program, version, 'N/A', compute_percentage(row[3], row[4]), compute_percentage(row[5], row[6])] + loadstats(version)
+# NOTE: uncomment/swap to omit lines left from table
+#        out = [program, version, 'N/A', compute_percentage(row[3], row[4])] + loadstats(version)
     assert(len(out) == len(outheader))
     return ','.join(out)
 
